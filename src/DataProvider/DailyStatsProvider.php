a827e0d9f7b797e1c83ed95695c41fae3226f07a
Łukasz Zychal <lukaszzpriv@gmail.com>
2022-06-24 11:35:29 +0200

    add dailystats

---
+++ src/DataProvider/DailyStatsProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\DataProvider;
+
+use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
+use App\Entity\DailyStats;
+use App\Repository\CheeseListingRepository;
+
+class DailyStatsProvider implements CollectionDataProviderInterface, RestrictedDataProviderInterface
+{
+
+    private $cheeseListingRepository;
+
+    public function __construct(CheeseListingRepository $cheeseListingRepository)
+    {
+        $this->cheeseListingRepository = $cheeseListingRepository;
+    }
+
+
+    public function getCollection(string $resourceClass, string $operationName = null)
+    {
+        $listings = $this->cheeseListingRepository
+            ->findBy([], [], 5);
+
+        $stats = new DailyStats(
+            new \DateTime(),
+            1000,
+            $listings
+        );
+        $stats2 = new DailyStats(
+            new \DateTime('-1 day'),
+            2000,
+            $listings
+        );
+        return [$stats, $stats2];
+    }
+    public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
+    {
+        return $resourceClass === DailyStats::class;
+    }
+}
---
+++ src/Entity/DailyStats.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Action\NotFoundAction;
+use DateTime;
+use DateTimeImmutable;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use DateTimeInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource(
+ *     normalizationContext={"groups"={"daily-stats:read"}},
+ *    itemOperations={
+ *         "get"={
+ *             "method"="GET",
+ *             "controller"=NotFoundAction::class,
+ *             "read"=false,
+ *             "output"=false,
+ *         },
+ *     },
+ *     collectionOperations={"get"}
+ * )
+ */
+class DailyStats
+{
+
+
+
+    /**
+     * @Groups({"daily-stats:read"})
+     */
+    public DateTimeInterface $date;
+
+    /**
+     * @Groups({"daily-stats:read"})
+     */
+    public int $totalVisitors;
+
+    /**
+     *  The 5 most popular cheese listings from this date!
+     * @Groups({"daily-stats:read"})
+     * @var array<CheeseListing>|CheeseListing[]
+     */
+    public array $mostPopularListings;
+
+    /**
+     * @param array|CheeseListing[] $mostPopularListings
+     */
+    public function __construct(\DateTimeInterface $date, int $totalVisitors, array $mostPopularListings)
+    {
+        $this->date = $date;
+        $this->totalVisitors = $totalVisitors;
+        $this->mostPopularListings = $mostPopularListings;
+    }
+
+
+    /**
+     * @ApiProperty(identifier=true)
+     */
+    public function getDateString(): string
+    {
+        return $this->date->format('Y-m-d');
+    }
+}
